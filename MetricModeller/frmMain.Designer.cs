5ffc908da0e10a0a355370e459517aa4dd6bb484
Dylan <dylandaviddavies@gmail.com>
2016-04-01 10:24:05 -0400

    Yadada
    Yadda

--- MetricModeller/frmMain.Designer.cs
+++ MetricModeller/frmMain.Designer.cs
@@ -23,20 +23,293 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.cbLines = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lblLines = new System.Windows.Forms.Label();
+            this.lblLanguage = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.lblPeople = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtInquiry = new System.Windows.Forms.TextBox();
+            this.txtMasterFiles = new System.Windows.Forms.TextBox();
+            this.txtInterfaces = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.cbInput = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // cbLines
+            // 
+            this.cbLines.FormattingEnabled = true;
+            this.cbLines.Location = new System.Drawing.Point(157, 52);
+            this.cbLines.Name = "cbLines";
+            this.cbLines.Size = new System.Drawing.Size(121, 21);
+            this.cbLines.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(157, 79);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // lblLines
+            // 
+            this.lblLines.AutoSize = true;
+            this.lblLines.Location = new System.Drawing.Point(97, 55);
+            this.lblLines.Name = "lblLines";
+            this.lblLines.Size = new System.Drawing.Size(54, 13);
+            this.lblLines.TabIndex = 2;
+            this.lblLines.Text = "# of Lines";
+            this.lblLines.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblLanguage
+            // 
+            this.lblLanguage.AutoSize = true;
+            this.lblLanguage.Location = new System.Drawing.Point(97, 82);
+            this.lblLanguage.Name = "lblLanguage";
+            this.lblLanguage.Size = new System.Drawing.Size(55, 13);
+            this.lblLanguage.TabIndex = 3;
+            this.lblLanguage.Text = "Language";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(157, 106);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 4;
+            // 
+            // lblPeople
+            // 
+            this.lblPeople.AutoSize = true;
+            this.lblPeople.Location = new System.Drawing.Point(89, 109);
+            this.lblPeople.Name = "lblPeople";
+            this.lblPeople.Size = new System.Drawing.Size(62, 13);
+            this.lblPeople.TabIndex = 5;
+            this.lblPeople.Text = "# of People";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(120, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Inquiry";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(95, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Master Files";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(104, 268);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Interfaces";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(164, 153);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 15;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(164, 180);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(100, 20);
+            this.txtOutput.TabIndex = 16;
+            // 
+            // txtInquiry
+            // 
+            this.txtInquiry.Location = new System.Drawing.Point(164, 207);
+            this.txtInquiry.Name = "txtInquiry";
+            this.txtInquiry.Size = new System.Drawing.Size(100, 20);
+            this.txtInquiry.TabIndex = 17;
+            // 
+            // txtMasterFiles
+            // 
+            this.txtMasterFiles.Location = new System.Drawing.Point(164, 237);
+            this.txtMasterFiles.Name = "txtMasterFiles";
+            this.txtMasterFiles.Size = new System.Drawing.Size(100, 20);
+            this.txtMasterFiles.TabIndex = 18;
+            // 
+            // txtInterfaces
+            // 
+            this.txtInterfaces.Location = new System.Drawing.Point(164, 265);
+            this.txtInterfaces.Name = "txtInterfaces";
+            this.txtInterfaces.Size = new System.Drawing.Size(100, 20);
+            this.txtInterfaces.TabIndex = 19;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(435, 288);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(72, 54);
+            this.btnCalculate.TabIndex = 20;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // cbInput
+            // 
+            this.cbInput.FormattingEnabled = true;
+            this.cbInput.Items.AddRange(new object[] {
+            "Simple (3)",
+            "Average (4)",
+            "Complex (6)"});
+            this.cbInput.Location = new System.Drawing.Point(270, 152);
+            this.cbInput.Name = "cbInput";
+            this.cbInput.Size = new System.Drawing.Size(121, 21);
+            this.cbInput.TabIndex = 21;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Simple (4)",
+            "Average (5)",
+            "Complex (7)"});
+            this.comboBox1.Location = new System.Drawing.Point(270, 179);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 22;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Simple (3)",
+            "Average (4)",
+            "Complex (6)"});
+            this.comboBox4.Location = new System.Drawing.Point(270, 207);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 23;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Simple (7)",
+            "Average (10)",
+            "Complex (15)"});
+            this.comboBox5.Location = new System.Drawing.Point(270, 237);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 24;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "Simple (5)",
+            "Average (7)",
+            "Complex (10)"});
+            this.comboBox6.Location = new System.Drawing.Point(270, 265);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6.TabIndex = 25;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.ClientSize = new System.Drawing.Size(519, 773);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbInput);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtInterfaces);
+            this.Controls.Add(this.txtMasterFiles);
+            this.Controls.Add(this.txtInquiry);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPeople);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.lblLanguage);
+            this.Controls.Add(this.lblLines);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.cbLines);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmMain";
             this.Text = "Metric Modeller";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbLines;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lblLines;
+        private System.Windows.Forms.Label lblLanguage;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label lblPeople;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtInquiry;
+        private System.Windows.Forms.TextBox txtMasterFiles;
+        private System.Windows.Forms.TextBox txtInterfaces;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ComboBox cbInput;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
     }
 }
 
--- MetricModeller/frmMain.cs
+++ MetricModeller/frmMain.cs
@@ -9,9 +9,41 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MetricModeller {
-    public partial class frmMain : Form {
+
+    public partial class frmMain : Form 
+    {
         public frmMain() {
             InitializeComponent();
+        }
+
+        private readonly int[] listOfWeights_input = new int[]{3, 4, 6};
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int[] listOfWeights_input = new int[] { 3, 4, 6 };
+
+            int num = listOfWeights_input[comboBox1.SelectedIndex];
         }
     }
 }
